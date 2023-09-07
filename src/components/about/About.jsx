a632e79de243f43246b538e766402f4bc328f7f5
ViggyC <vich7304@colorado.edu>
2023-09-06 20:51:20 -0600

    help

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import "./About.css";
 import grad from "../../assets/images/vignesh_grad_pic1024_1.jpg";
-import { HiOutlineLightBulb } from "react-icons/hi";
-import { GrProjects } from "react-icons/gr";
-import { BiCodeCurly } from "react-icons/bi";
-import { AiFillFolderOpen } from "react-icons/ai";
 
 const About = () => {
   return (
