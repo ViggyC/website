72d27335c32a896a4076354d877061de87640e29
ViggyC <vich7304@colorado.edu>
2024-01-30 15:58:14 -0700

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ import Footer from "./components/footer/Footer";
 import OpenAIChatComponent from "./components/gpt/GPT";
 import Email from "./components/contact2/Email";
 
+//
+
 const App = () => {
   return (
     <>
