c54b28ff1f639c8a3f6ab130580194d3f9eff9ee
ViggyC <vich7304@colorado.edu>
2024-02-02 19:34:15 -0700

    hash router

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-import ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import Camera from "./components/media/camera/Camera";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App />,
   },
   {
-    path: "/media",
+    path: "/blog",
     element: <Camera />,
   },
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-// ReactDOM.render(<App />, document.querySelector("#root"));
-root.render(<RouterProvider router={router} />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
