79c45cc149f2ee5fa595c8360e54feb2a8f401ad
ViggyC <vich7304@colorado.edu>
2024-03-21 18:25:56 -0600

    skill issues

---
+++ src/assets/data/skills.js
@@ -0,0 +1,60 @@
+const logos = [
+  {
+    id: 1,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original-wordmark.svg",
+  },
+  {
+    id: 10,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg",
+  },
+  {
+    id: 2,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg",
+  },
+  {
+    id: 13,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/azuresqldatabase/azuresqldatabase-original.svg",
+  },
+  {
+    id: 3,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bash/bash-original.svg",
+  },
+  {
+    id: 4,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linux/linux-original.svg",
+  },
+  {
+    id: 5,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original-wordmark.svg",
+  },
+  {
+    id: 6,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/angular/angular-original.svg",
+  },
+  {
+    id: 7,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg",
+  },
+  {
+    id: 8,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bamboo/bamboo-original-wordmark.svg",
+  },
+  {
+    id: 15,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-plain-wordmark.svg",
+  },
+  {
+    id: 14,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jupyter/jupyter-original-wordmark.svg",
+  },
+  {
+    id: 9,
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/googlecloud/googlecloud-original-wordmark.svg",
+  },
+  {
+    id: 11,
+    src: "https://res.cloudinary.com/dxi2scgya/images/f_auto,q_auto/v1685190296/snowflake/snowflake.svg?_i=AA",
+  },
+];
+
+export default logos;
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -2,143 +2,17 @@ import React from "react";
 import "./Skills.css";
 import { BsFillPatchCheckFill } from "react-icons/bs";
 import Certs from "../certs/Certs";
+import logos from "../../assets/data/skills";
 
 const Skills = () => {
   return (
     <section id="skills">
       <h2>Technical Skills</h2>
-
       <div className="container skills_container">
-        <div className="skills_programming">
-          <h3>Programming</h3>
-          <div className="skills_content">
-            <article className="skills_details">
-              <BsFillPatchCheckFill className="skills_icon" />
-              <div>
-                <h4 className="skill_name">C</h4>
-              </div>
-            </article>
-            <article className="skills_details">
-              <BsFillPatchCheckFill className="skills_icon" />
-              <div>
-                <h4 className="skill_name">Python</h4>
-              </div>
-            </article>
-            <article className="skills_details">
-              <BsFillPatchCheckFill className="skills_icon" />
-              <div>
-                <h4 className="skill_name">Bash</h4>
-              </div>
-            </article>
-            <article className="skills_details">
-              <BsFillPatchCheckFill className="skills_icon" />
-              <div>
-                <h4 className="skill_name">Javascript</h4>
-              </div>
-            </article>
-            <article className="skills_details">
-              <BsFillPatchCheckFill className="skills_icon" />
-              <div>
-                <h4 className="skill_name">SQL</h4>
-              </div>
-            </article>
-            <article className="skills_details">
-              <BsFillPatchCheckFill className="skills_icon" />
-              <div>
-                <h4 className="skill_name">React</h4>
-              </div>
-            </article>
-            <article className="skills_details">
-              <BsFillPatchCheckFill className="skills_icon" />
-              <div>
-                <h4 className="skill_name">Angular</h4>
-              </div>
-            </article>
-          </div>
-        </div>
-        {/* Backend */}
-        <div className="skills_backend">
-          <h3>Backend</h3>
-          <div className="skills_content">
-            <article className="skills_details">
-              <BsFillPatchCheckFill className="skills_icon" />
-              <div>
-                <h4 className="skill_name">NodeJS</h4>
-              </div>
-            </article>
-            <article className="skills_details">
-              <BsFillPatchCheckFill className="skills_icon" />
-              <div>
-                <h4 className="skill_name">MongoDB</h4>
-              </div>
-            </article>
-            <article className="skills_details">
-              <BsFillPatchCheckFill className="skills_icon" />
-              <div>
-                <h4 className="skill_name">MySQL</h4>
-              </div>
-            </article>
-            <article className="skills_details">
-              <BsFillPatchCheckFill className="skills_icon" />
-              <div>
-                <h4 className="skill_name">.NETCore</h4>
-              </div>
-            </article>
-            <article className="skills_details">
-              <BsFillPatchCheckFill className="skills_icon" />
-              <div>
-                <h4 className="skill_name">PostgreSQL</h4>
-              </div>
-            </article>
-          </div>
-        </div>
-        {/* Courses */}
-        <div className="skills_services">
-          <h3>Platforms & Services</h3>
-          <div className="skills_content">
-            <article className="skills_details">
-              <BsFillPatchCheckFill className="skills_icon" />
-              <div>
-                <h4 className="skill_name">Heroku</h4>
-              </div>
-            </article>
-            <article className="skills_details">
-              <BsFillPatchCheckFill className="skills_icon" />
-              <div>
-                <h4 className="skill_name">Fly.io</h4>
-              </div>
-            </article>
-            <article className="skills_details">
-              <BsFillPatchCheckFill className="skills_icon" />
-              <div>
-                <h4 className="skill_name">AWS</h4>
-              </div>
-            </article>
-            <article className="skills_details">
-              <BsFillPatchCheckFill className="skills_icon" />
-              <div>
-                <h4 className="skill_name">GCP</h4>
-              </div>
-            </article>
-            <article className="skills_details">
-              <BsFillPatchCheckFill className="skills_icon" />
-              <div>
-                <h4 className="skill_name">Bamboo</h4>
-              </div>
-            </article>
-            <article className="skills_details">
-              <BsFillPatchCheckFill className="skills_icon" />
-              <div>
-                <h4 className="skill_name">GitHub</h4>
-              </div>
-            </article>
-            <article className="skills_details">
-              <BsFillPatchCheckFill className="skills_icon" />
-              <div>
-                <h4 className="skill_name">Docker</h4>
-              </div>
-            </article>
-          </div>
+        <div className="skills_image_container">
+          {logos.map((logo) => (
+            <img key={logo.id} src={logo.src} alt={logo.alt} className="logo" />
+          ))}
         </div>
       </div>
       <Certs />
